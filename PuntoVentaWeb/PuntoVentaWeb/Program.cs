15a8a812cb0651ada3d9806891ee3976c5d79f24
Axel Noel Cabezas Cerda <114128602+AxelCab17@users.noreply.github.com>
2024-06-03 21:01:31 -0600

    Revert "Login P1"
    This reverts commit 901e82338637b676de94bd0780a99834ddea1e73.

--- PuntoVentaWeb/PuntoVentaWeb/Program.cs
+++ PuntoVentaWeb/PuntoVentaWeb/Program.cs
@@ -1,12 +1,4 @@
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using PuntoVentaWeb.Areas.Identity.Data;
 var builder = WebApplication.CreateBuilder(args);
-var connectionString = builder.Configuration.GetConnectionString("PuntoVentaDBContextConnection") ?? throw new InvalidOperationException("Connection string 'PuntoVentaDBContextConnection' not found.");
-
-builder.Services.AddDbContext<PuntoVentaDBContext>(options => options.UseSqlServer(connectionString));
-
-builder.Services.AddDefaultIdentity<PuntoVentaUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<PuntoVentaDBContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -31,7 +23,5 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.MapRazorPages();
 
 app.Run();
