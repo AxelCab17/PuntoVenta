3900c14a7901efd9a1c38d67105ed637253c0f12
Axel Noel Cabezas Cerda <114128602+AxelCab17@users.noreply.github.com>
2024-06-30 19:00:11 -0600

    END Carrito

--- PuntoVentaAPI/PuntoVentaAPI/Entities/CarritoEnt.cs
+++ PuntoVentaAPI/PuntoVentaAPI/Entities/CarritoEnt.cs
@@ -5,5 +5,11 @@ namespace PuntoVentaAPI.Entities
     public class Carrito
     {
         public List<CarritoItemEnt> Items { get; set; } = new List<CarritoItemEnt>();
+
+        public decimal Subtotal => Items.Sum(item => item.Producto.Precio * item.Cantidad);
+
+        public decimal IVA => Subtotal * 0.13m;  // 13% IVA
+
+        public decimal Total => Subtotal + IVA;
     }
 }
